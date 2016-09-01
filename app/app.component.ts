e37aaff329c6d84454ddf3c7429aaafdd235397d
Shu Yu <shu.yu@omnilogic.net>
2016-08-31 22:49:13 -0600

    * Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/app.component.html'
+})
+export class AppComponent {
+
+  //[] means property binding - Component to DOM
+  //() means event binding - DOM to Component
+  title = 'Customer App';
+  name = 'Ward';
+  wardsColor = 'blue';
+
+  changeSuitColor() {
+    this.wardsColor = this.wardsColor==='blue' ? 'red' : 'blue';
+  }
+}
